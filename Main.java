599e24713666faf212791cf2c555337e7ebc1868
Tsegmid <tsegmid.d@novelsoft.mn>
2023-09-09 12:17:22 +0800

    test comment

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // test
         Scanner sc = new Scanner(System.in);
         int number = Integer.parseInt(sc.nextLine());
         int number2 = 2;
